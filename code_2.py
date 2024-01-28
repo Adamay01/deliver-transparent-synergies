fc5cd04470c53b252c0cf2ca3c566c5b097f8a47
Ada <139880994+Adamay01@users.noreply.github.com>
2024-01-28 23:20:12 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,33 @@
+
+main();
+}
+    const data = [];
+function generateRandomData() {
+    });
+
+    const data = generateRandomData();
+        data.push(Math.floor(Math.random() * 100) + 1);
+    data.forEach(item => {
+        console.log("Random Number: " + item);
+
+    return data;
+    for (let i = 0; i < 10; i++) {
+    }
+}
+function main() {    });
+    }
+main();
+}
+function main() {
+
+    return data;
+}
+        console.log("Random Number: " + item);
+function generateRandomData() {
+    for (let i = 0; i < 10; i++) {
+    const data = generateRandomData();
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+
+    data.forEach(item => {
+    const data = [];
